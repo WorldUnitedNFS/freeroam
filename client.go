17825aebce49614d7aae688822687316c5b07860
Steven M. <34949863+SPEED0U@users.noreply.github.com>
2025-09-29 18:23:34 +0200

    Player radius sync

--- client.go
+++ client.go
@@ -8,7 +8,6 @@ import (
 	"bytes"
 	"encoding/binary"
 	"fmt"
-	"github.com/WorldUnitedNFS/freeroam/grid"
 	"github.com/WorldUnitedNFS/freeroam/math"
 	"net"
 	"runtime/debug"
@@ -37,54 +36,55 @@ func (self clientPosSort) Less(i, j int) bool {
 }
 
 type ClientConfig struct {
-	InitialTick     uint16
-	Addr            *net.UDPAddr
-	Conn            *net.UDPConn
-	Buffers         *sync.Pool
-	Clients         map[string]*Client
-	AllowedPersonas []int
+	InitialTick       uint16
+	Addr              *net.UDPAddr
+	Conn              *net.UDPConn
+	Buffers           *sync.Pool
+	Clients           map[string]*Client
+	AllowedPersonas   []int
+	VisibilityRadius  float64
+	MaxVisiblePlayers int
 }
 
 func newClient(opts ClientConfig) *Client {
 	c := &Client{
-		Addr:            opts.Addr,
-		conn:            opts.Conn,
-		startTime:       time.Now(),
-		initialTick:     opts.InitialTick,
-		tickDiff:        int16(opts.InitialTick - getServerTick()),
-		seq:             0,
-		slots:           make([]*slotInfo, 14),
-		LastPacket:      time.Now(),
-		clients:         opts.Clients,
-		allowedPersonas: opts.AllowedPersonas,
-		buffers:         opts.Buffers,
-		updateID:        1,
+		Addr:             opts.Addr,
+		conn:             opts.Conn,
+		startTime:        time.Now(),
+		initialTick:      opts.InitialTick,
+		tickDiff:         int16(opts.InitialTick - getServerTick()),
+		seq:              0,
+		slots:            make([]*slotInfo, opts.MaxVisiblePlayers),
+		LastPacket:       time.Now(),
+		clients:          opts.Clients,
+		allowedPersonas:  opts.AllowedPersonas,
+		buffers:          opts.Buffers,
+		updateID:         1,
+		visibilityRadius: opts.VisibilityRadius,
 	}
 	return c
 }
 
 type Client struct {
-	Addr            *net.UDPAddr
-	conn            *net.UDPConn
-	startTime       time.Time
-	initialTick     uint16
-	tickDiff        int16
-	seq             uint16
-	carPos          CarPosPacket
-	chanInfo        []byte
-	playerInfo      []byte
-	slots           []*slotInfo
-	LastPacket      time.Time
-	PersonaName     string
-	allowedPersonas []int
-	ackMissedCount  int
-	updateID        uint8
-	buffers         *sync.Pool
-	clients         map[string]*Client
-	posRecvTD       uint16
-	posX            float32
-	posY            float32
-	currentGridCell *grid.WorldGridCell
+	Addr             *net.UDPAddr
+	conn             *net.UDPConn
+	startTime        time.Time
+	initialTick      uint16
+	tickDiff         int16
+	seq              uint16
+	carPos           CarPosPacket
+	chanInfo         []byte
+	playerInfo       []byte
+	slots            []*slotInfo
+	LastPacket       time.Time
+	PersonaName      string
+	allowedPersonas  []int
+	ackMissedCount   int
+	updateID         uint8
+	buffers          *sync.Pool
+	clients          map[string]*Client
+	posRecvTD        uint16
+	visibilityRadius float64
 }
 
 func (c *Client) registerUpdate() {
@@ -135,9 +135,6 @@ func (c *Client) processPacket(packet []byte) {
 		}
 	}()
 	c.LastPacket = time.Now()
-	c.posX = float32(binary.BigEndian.Uint16(packet[14:16])+350*5) / 5
-	c.posY = -float32(int16(binary.BigEndian.Uint16(packet[12:14])-5000*5)) / 5
-
 	srvCounter := binary.BigEndian.Uint16(packet[8:10])
 	for _, slot := range c.slots {
 		if slot != nil && !slot.UpdateACKed {
@@ -199,20 +196,25 @@ func (c *Client) processPacket(packet []byte) {
 	}
 }
 
-func (c *Client) getClosestPlayers(clients map[*Client]bool) []*Client {
+func (c *Client) getClosestPlayers(clients []*Client) []*Client {
 	closePlayers := make([]clientPosSortInfo, 0)
-	for client := range clients {
+	for _, client := range clients {
 		if !client.IsReady() || client.Addr == c.Addr {
 			continue
 		}
 		distance := math.Distance(c.GetPos(), client.GetPos())
+		if distance > c.visibilityRadius {
+			continue
+		}
 		closePlayers = append(closePlayers, clientPosSortInfo{
 			Length: int(distance),
 			Client: client,
 		})
 	}
 	sort.Sort(clientPosSort(closePlayers))
-	out := make([]*Client, min(14, len(closePlayers)))
+	
+	maxSlots := len(c.slots)
+	out := make([]*Client, min(maxSlots, len(closePlayers)))
 	for i := range out {
 		out[i] = closePlayers[i].Client
 	}
@@ -247,7 +249,7 @@ func (c *Client) addSlot(client *Client) {
 	}
 }
 
-func (c *Client) recalculateSlots(clients map[*Client]bool) {
+func (c *Client) recalculateSlots(clients []*Client) {
 	players := c.getClosestPlayers(clients)
 	oldPlayers := make([]*Client, 0)
 	for _, slot := range c.slots {
@@ -272,6 +274,15 @@ func (c *Client) recalculateSlots(clients map[*Client]bool) {
 }
 
 func (c *Client) sendPlayerSlots() {
+	clients := make([]*Client, len(c.clients))
+	{
+		i := 0
+		for _, cl := range c.clients {
+			clients[i] = cl
+			i++
+		}
+	}
+	c.recalculateSlots(clients)
 	buf := c.buffers.Get().(*bytes.Buffer)
 	buf.Reset()
 	seq := c.getSeq()
--- server.go
+++ server.go
@@ -7,40 +7,30 @@ package freeroam
 import (
 	"bytes"
 	"encoding/binary"
-	"github.com/WorldUnitedNFS/freeroam/grid"
 	"log"
 	"net"
 	"sync"
 	"time"
 )
 
-func NewServer() *Server {
-	worldGrid := grid.CreateWorldGrid(2048, 1125, 3)
-	gridCellOccupants := make([]map[*Client]bool, worldGrid.NumCells())
-
-	for i := 0; i < worldGrid.NumCells(); i++ {
-		gridCellOccupants[i] = make(map[*Client]bool)
-	}
-
+func NewServer(config Config) *Server {
 	return &Server{
 		Clients: make(map[string]*Client),
 		recvbuf: make([]byte, 1024),
 		buffers: &sync.Pool{
 			New: func() interface{} { return new(bytes.Buffer) },
 		},
-		worldGrid:         worldGrid,
-		gridCellOccupants: gridCellOccupants,
+		config: config,
 	}
 }
 
 type Server struct {
 	sync.Mutex
-	listener          *net.UDPConn
-	Clients           map[string]*Client
-	recvbuf           []byte
-	buffers           *sync.Pool
-	worldGrid         grid.WorldGrid
-	gridCellOccupants []map[*Client]bool
+	listener *net.UDPConn
+	Clients  map[string]*Client
+	recvbuf  []byte
+	buffers  *sync.Pool
+	config   Config
 }
 
 func (i *Server) Listen(addrStr string) error {
@@ -64,11 +54,13 @@ func (i *Server) RunPacketRead() {
 		if len(data) == 58 && data[2] == 0x06 {
 			log.Printf("New client from %v", addr.String())
 			client := newClient(ClientConfig{
-				InitialTick: binary.BigEndian.Uint16(data[52:54]),
-				Addr:        addr,
-				Conn:        i.listener,
-				Buffers:     i.buffers,
-				Clients:     i.Clients,
+				InitialTick:       binary.BigEndian.Uint16(data[52:54]),
+				Addr:              addr,
+				Conn:              i.listener,
+				Buffers:           i.buffers,
+				Clients:           i.Clients,
+				VisibilityRadius:  i.config.UDP.VisibilityRadius,
+				MaxVisiblePlayers: i.config.UDP.MaxVisiblePlayers,
 			})
 			i.Clients[addr.String()] = client
 			client.replyHandshake()
@@ -83,44 +75,13 @@ func (i *Server) RunPacketRead() {
 	}
 }
 
-func mapGamePosToWorldMapPos(x, y float32) (mx, my float32) {
-	mx = 0.183583939*x - 10.0328626
-	my = -0.183613514*y + 773.060633
-	return
-}
-
 func (i *Server) RunTimer() {
 	for {
 		i.Lock()
 		for k, client := range i.Clients {
-			currentCell := client.currentGridCell
 			if !client.Active() {
 				log.Printf("Removing inactive client %v", client.Addr.String())
-
-				if currentCell != nil {
-					delete(i.gridCellOccupants[currentCell.ID], client)
-				}
-
 				delete(i.Clients, k)
-			} else if client.IsReady() {
-				mappedX, mappedY := mapGamePosToWorldMapPos(client.posX, client.posY)
-				newCell := i.worldGrid.FindCell(mappedX, mappedY)
-				if newCell == nil {
-					log.Printf("Could not find cell for client %v at (%f, %f) [WM: (%f, %f)]", client.Addr, client.posX, client.posY, mappedX, mappedY)
-				} else if currentCell != newCell {
-					if currentCell != nil {
-						delete(i.gridCellOccupants[currentCell.ID], client)
-					}
-
-					log.Printf("Client %v is now in cell %d", client.Addr, newCell.ID)
-					i.gridCellOccupants[newCell.ID][client] = true
-					client.currentGridCell = newCell
-					currentCell = newCell
-				}
-
-				if currentCell != nil {
-					client.recalculateSlots(i.gridCellOccupants[currentCell.ID])
-				}
 			}
 		}
 		i.Unlock()
